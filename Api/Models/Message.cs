08949aa93d6140d0dcfd1bd648a5ca418ad9ed63
Erik <erik.vitelli1@gmail.com>
2021-03-06 18:21:50 -0300

    Criação dos modelos

---
+++ Api/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    [Table("Messages")]
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+     
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public int SenderId { get; set; }
+
+        public virtual User Sender { get; set; }
+    }
+}
---
+++ Api/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public  int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        public virtual List<Message> Messages{ get; set; }
+    }
+}
