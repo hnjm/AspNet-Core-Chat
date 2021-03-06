8f71a26ef6c8ae642c99bfd7acdaf8c13811ef3c
Erik <erik.vitelli1@gmail.com>
2021-03-06 19:56:44 -0300

    Criação dos endpoints de mensagens

---
+++ Api/Controllers/MessagesController.cs
@@ -0,0 +1,37 @@
+﻿using Api.Database;
+using Api.Models;
+using Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        public MessageRepository Repository { get; set; }
+
+        public MessagesController(Context context)
+        {
+            Repository = new MessageRepository(context);
+        }
+
+        [HttpPost]
+        public async Task<Message> Create([FromBody] Message message)
+        {
+            await Repository.Create(message);
+            return message;
+        }
+
+        [HttpGet]
+        public async Task<List<Message>> List()
+        {
+            return await Repository.List();
+        }
+    }
+}
---
+++ Api/Repositories/MessageRepository.cs
@@ -0,0 +1,34 @@
+﻿using Api.Database;
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public class MessageRepository
+    {
+        public Context Context;
+
+        public MessageRepository(Context context)
+        {
+            Context = context;
+        }
+
+        public async Task Create(Message message)
+        {
+            Context.Messages.Add(message);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task<List<Message>> List()
+        {
+            return await Context.Messages.AsNoTracking().ToListAsync();
+        }
+
+
+
+    }
+}
