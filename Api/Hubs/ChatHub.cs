c76bb45230054782f2d70ece6ebf18b48c99e3ec
Erik <erik.vitelli1@gmail.com>
2021-03-06 23:15:37 -0300

    Adicção dos métodos de envio de mensagens

--- Api/Hubs/ChatHub.cs
+++ Api/Hubs/ChatHub.cs
@@ -15,23 +15,14 @@ namespace Api.Hubs
 
         public override Task OnConnectedAsync()
         {
-            var user = JsonConvert.DeserializeObject<User>(Context.GetHttpContext().Request.Query["user"]);
-            Users.Add(user);
-            //Ao usar o método All eu estou enviando a mensagem para todos os usuários conectados no meu Hub
-            Clients.All.SendAsync("chat", Users, user);
+            // Conecta o usuário ao hub
             return base.OnConnectedAsync();
         }
 
-
-        /// <summary>
-        /// Método responsável por encaminhar as mensagens pelo hub
-        /// </summary>
-        /// <param name="message">Este parâmetro é nosso objeto representando a mensagem e os usuários envolvidos</param>
-        /// <returns></returns>
         public async Task SendMessage(Message message)
         {
-            //Ao usar o método Client(_connections.GetUserId(chat.destination)) eu estou enviando a mensagem apenas para o usuário destino, não realizando broadcast
-            await Clients.Client("1").SendAsync("Receive", message.Sender, message.Text);
+            // Envia uma mensagem para todos os usuários registrados
+            await Clients.All.SendAsync("ReceberMensagem", message);
         }
     }
 }
